dc37503de7f6617f1985abc270d3e9c13da812dd
Aleksandr Lesnenko <alxnddr@gmail.com>
2022-04-13 03:20:39 +0400

    data model ui for json fields

--- frontend/src/metabase-types/api/table.ts
+++ frontend/src/metabase-types/api/table.ts
@@ -1,6 +1,22 @@
+import { Field } from "./field";
+
+export type VisibilityType =
+  | null
+  | "details-only"
+  | "hidden"
+  | "normal"
+  | "retired"
+  | "sensitive"
+  | "technical"
+  | "cruft";
+
 export interface Table {
   id: number;
   db_id: number;
   name: string;
+  description: string | null;
+  display_name: string;
   schema: string;
+  visibility_type: VisibilityType;
+  fields?: Field[];
 }
